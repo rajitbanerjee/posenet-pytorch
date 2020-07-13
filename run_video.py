3678c8611af94e6d4a73e32e559c330172e6d5b3
Rajit Banerjee <rajit@gradient.ucd.ie>
2020-07-13 19:58:15 +0000

    minor path change

--- run_video.py
+++ run_video.py
@@ -12,7 +12,7 @@ parser.add_argument('--notxt', action='store_true')
 parser.add_argument('--input', type=str,
                     default='../../assets/videos/jumping_jacks.mp4')
 parser.add_argument('--output', type=str,
-                    default='../../src/posenet-py/output/')
+                    default='./output/')
 parser.add_argument('--scale_factor', type=float, default=1.0)
 args = parser.parse_args()
 
